39044d7751e37205de0377484a01bedb23382327
EmyFicherova <106160486+EmyFicherova@users.noreply.github.com>
2022-05-24 16:59:20 +0300

    Back in 30 minutes

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int mins = int.Parse(Console.ReadLine());
+
+            mins += 30;
+
+            if (mins >= 60)
+            {
+                mins -= 60;
+                hours++;
+            }
+            if (hours == 24)
+            {
+                hours = 0;
+            }
+
+            Console.WriteLine($"{hours}:{mins:d2}");
+
+        }
+    }
+}
