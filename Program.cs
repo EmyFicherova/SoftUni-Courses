4d27f65e88aab3fe96e24bf100a1cae66b676368
EmyFicherova <106160486+EmyFicherova@users.noreply.github.com>
2022-05-24 17:05:14 +0300

    Student information

--- Program.cs
+++ Program.cs
@@ -1,21 +1,16 @@
 ﻿using System;
 
-namespace Passed_or_Failed
+namespace Student_Information
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double grade = double.Parse(Console.ReadLine());
+            string studentName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
 
-            if (grade >= 3)
-            {
-                Console.WriteLine("Passed!");
-            }
-            else
-            {
-                Console.WriteLine("Failed!");
-            }
+            Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {averageGrade:f2} ");
         }
     }
 }
